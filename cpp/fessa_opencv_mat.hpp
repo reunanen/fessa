e0bd04e8dd8a84e13b42835a66f5c98b056536b5
Juha Reunanen <juha.reunanen@tomaattinen.com>
2017-08-20 12:17:22 +0300

    Add MaskedMatFilter

--- cpp/fessa_opencv_mat.hpp
+++ cpp/fessa_opencv_mat.hpp
@@ -43,6 +43,62 @@ namespace fessa
         int type;
         cv::Mat s;
     };
+
+    // Like MatFilter, but accepts a mask when filtering
+    template <typename A = double>
+    class MaskedMatFilter {
+    public:
+        MaskedMatFilter(A baseAlpha, int type = CV_64FC1)
+            : baseAlpha(baseAlpha), type(type)
+        {
+            assert(type != 0);
+        }
+
+        // Process a new image x, and return the filtered image s
+        const cv::Mat& filter(cv::InputArray x, cv::InputArray mask) {
+            if (x.size() != mask.size()) { // TODO: allow mask to be cv::noArray
+                throw std::runtime_error("Invalid dimensions");
+            }
+            const bool startOver 
+                = s.type() != type || s.size() != x.size()
+                || a.type() != type || a.size() != x.size()
+                || d.type() != type || d.size() != x.size();
+            if (startOver) {
+                a.create(x.getMat().size(), type);
+                a.setTo(1);
+                d.create(x.getMat().size(), type);
+                d.setTo(0);
+                s.create(x.getMat().size(), type);
+                s.setTo(0);
+            }
+
+            // update d
+            cv::add(d, a, d, mask);
+
+            // update a
+            cv::multiply(a, 1 - baseAlpha, temp);
+            temp.copyTo(a, mask);
+
+            // calculate alpha and 1 - alpha
+            cv::divide(1.0, d, temp); // temp is now alpha
+            cv::subtract(1.0, temp, temp2); // temp2 is now 1 - alpha
+
+            // do the weighting
+            cv::multiply(x, temp, temp, 1.0, type); // temp is now alpha * x
+            cv::multiply(s, temp2, temp2); // temp2 is now (1 - alpha) * s
+
+            // calculate the filtered result
+            cv::add(temp, temp2, s, mask);
+            return s;
+        }
+    private:
+        A baseAlpha;
+        int type;
+        cv::Mat s;
+        cv::Mat a;
+        cv::Mat d;
+        cv::Mat temp, temp2;
+    };
 }
 
 #endif // FESSA_OPENCV_MAT
